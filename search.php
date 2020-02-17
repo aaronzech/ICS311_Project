d4a20c974cd7ffdcb100d17445003dab7ab8b206
Zech <ZechAaron@district279.org>
2020-02-17 11:28:13 -0600

    Table printing

--- search.php
+++ search.php
@@ -1,5 +1,9 @@
 
-
+<style>
+table, th, td {
+    border: 1px solid black;
+}
+</style>
 <h1>Search Results</h1>
 
 
@@ -18,45 +22,47 @@
 		// Whatever was in the search box
 		$search_query = $_POST['search'];
 		
-				
+		// Create table to display the results
+		echo "<table><tr><th>Title</th><th>ISBN</th><th>Author Last Name</th><th>Author First Name</th></tr>";
+		
+		
+		// Display Format
+		echo "<h2>$search_type Results</h2>";
+		
 		// ISBN Search Selected
 		if($search_type == "ISBN"){
 			
 			// Search by title only
 			$query = mysql_query("SELECT * FROM book WHERE ISBN like '%$search_query%'") or die ("could not find by ISBN search");
-				$count = mysql_num_rows($query);
+			$count = mysql_num_rows($query);
 		
 		if($count==0){
 			$output = 'There was no search results';
 		}
 		else{
+			
+			
 			while($row = mysql_fetch_array($query)){
+				
+				
 				$title = $row['Title'];
 				$ISBN = $row['ISBN'];
 				$AuthorLastName=$row['Author_LastName'];
 				$AuthorFirstName=$row['Author_FirstName'];
 				
+				// populate the table with data
+				 echo "<tr><td>".$row["Title"]."</td><td>".$row["ISBN"]."</td><td>".$row["Author_LastName"]."</td><td>".$row["Author_FirstName"]."</td></tr>";
+				
+				
 				// Display Result
 				$output = $ISBN;				
-			}
+			} 
 		}
 		
-		// Display Format
-		echo "<h2>$search_type Results</h2>";
-		echo "<br>";
-		echo "ISBN: ";
-		print("$output");
-		echo "<br>";
-		echo "Title: ";
-		print("$title");
-		echo "<br>";
-		echo "Author's Last Name: ";
-		print("$AuthorLastName");
-		echo "<br>";
-		echo "Author's First Name: ";
-		print("$AuthorFirstName");
 		
 		}
+		
+		// Title Search Selected
 		else if($search_type == "Title"){
 			echo "Title choosen";
 			$query = mysql_query("SELECT * FROM book WHERE TITLE like '%$search_query%'") or die ("could not by Title search");
@@ -69,7 +75,8 @@
 			while($row = mysql_fetch_array($query)){
 				$title = $row['Title'];
 				
-				
+				// populate the table with data
+				 echo "<tr><td>".$row["Title"]."</td><td>".$row["ISBN"]."</td><td>".$row["Author_LastName"]."</td><td>".$row["Author_FirstName"]."</td></tr>";
 				$output = $title;
 				
 				
@@ -77,9 +84,10 @@
 		}
 		print("$output");
 		}
+		// Author Last Name Search 
 		else if($search_type == "AuthorLastName"){
 			echo "Author's Last Name choosen";
-				$query = mysql_query("SELECT * FROM book WHERE AuthorLastName like '%$search_query%'") or die ("could not find by Author's Last Name search");
+				$query = mysql_query("SELECT * FROM book WHERE Author_LastName like '%$search_query%'") or die ("could not find by Author's Last Name search");
 					$count = mysql_num_rows($query);
 		
 		if($count==0){
@@ -89,7 +97,8 @@
 			while($row = mysql_fetch_array($query)){
 				$title = $row['Title'];
 				
-				
+				// populate the table with data
+				 echo "<tr><td>".$row["Title"]."</td><td>".$row["ISBN"]."</td><td>".$row["Author_LastName"]."</td><td>".$row["Author_FirstName"]."</td></tr>";
 				$output = $title;
 				
 				
@@ -99,7 +108,23 @@
 		print("$output");
 		}
 		else if($search_type == "AuthorFirstName"){
-			echo "Author's First Name choosen";
+			
+				$query = mysql_query("SELECT * FROM book WHERE Author_FirstName like '%$search_query%'") or die ("could not find by Author's Last Name search");
+				$count = mysql_num_rows($query);
+		
+		if($count==0){
+			$output = 'There was no search results';
+		}
+		else{
+			while($row = mysql_fetch_array($query)){
+				$title = $row['Title'];
+				
+				// populate the table with data
+				 echo "<tr><td>".$row["Title"]."</td><td>".$row["ISBN"]."</td><td>".$row["Author_LastName"]."</td><td>".$row["Author_FirstName"]."</td></tr>";
+				$output = $title;
+				
+				
+			}
 		}
 		
 		
@@ -112,5 +137,5 @@
 		
 	
 	}
-
+	}
 ?>
