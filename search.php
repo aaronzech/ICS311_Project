e22c11f88d6e4cdef18ceba28f59ab1b7940295a
Zech <ZechAaron@district279.org>
2020-02-17 10:55:02 -0600

    Update 1 add drop down search form

--- search.php
+++ search.php
@@ -1,6 +1,6 @@
 
 
-<h1>Book Title Search Results</h1>
+<h1>Search Results</h1>
 
 
 <?php
@@ -11,13 +11,56 @@
 
 	// Run SQL 
 	if(isset($_POST['search'])){
+		
+		// Grab the desired search type
+		$search_type = ($_POST["searchType"]);
+		
+		// Whatever was in the search box
 		$search_query = $_POST['search'];
 		
+				
+		// ISBN Search Selected
+		if($search_type == "ISBN"){
+			
+			// Search by title only
+			$query = mysql_query("SELECT * FROM book WHERE ISBN like '%$search_query%'") or die ("could not find by ISBN search");
+				$count = mysql_num_rows($query);
 		
-		// Search by title only
-		$query = mysql_query("SELECT * FROM book WHERE TITLE like '%$search_query%'") or die ("could not search");
+		if($count==0){
+			$output = 'There was no search results';
+		}
+		else{
+			while($row = mysql_fetch_array($query)){
+				$title = $row['Title'];
+				$ISBN = $row['ISBN'];
+				$AuthorLastName=$row['Author_LastName'];
+				$AuthorFirstName=$row['Author_FirstName'];
+				
+				// Display Result
+				$output = $ISBN;				
+			}
+		}
 		
-		$count = mysql_num_rows($query);
+		// Display Format
+		echo "<h2>$search_type Results</h2>";
+		echo "<br>";
+		echo "ISBN: ";
+		print("$output");
+		echo "<br>";
+		echo "Title: ";
+		print("$title");
+		echo "<br>";
+		echo "Author's Last Name: ";
+		print("$AuthorLastName");
+		echo "<br>";
+		echo "Author's First Name: ";
+		print("$AuthorFirstName");
+		
+		}
+		else if($search_type == "Title"){
+			echo "Title choosen";
+			$query = mysql_query("SELECT * FROM book WHERE TITLE like '%$search_query%'") or die ("could not by Title search");
+				$count = mysql_num_rows($query);
 		
 		if($count==0){
 			$output = 'There was no search results';
@@ -33,6 +76,41 @@
 			}
 		}
 		print("$output");
+		}
+		else if($search_type == "AuthorLastName"){
+			echo "Author's Last Name choosen";
+				$query = mysql_query("SELECT * FROM book WHERE AuthorLastName like '%$search_query%'") or die ("could not find by Author's Last Name search");
+					$count = mysql_num_rows($query);
+		
+		if($count==0){
+			$output = 'There was no search results';
+		}
+		else{
+			while($row = mysql_fetch_array($query)){
+				$title = $row['Title'];
+				
+				
+				$output = $title;
+				
+				
+			}
+		}
+		
+		print("$output");
+		}
+		else if($search_type == "AuthorFirstName"){
+			echo "Author's First Name choosen";
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
 	}
 
 ?>
